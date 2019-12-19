998b6f889e8b8e43b2352fe49c2f8a920b271865
wangweeii <wangweei@gmail.com>
2019-12-19 17:52:28 +0800

    Init

---
+++ main.cc
@@ -0,0 +1,19 @@
+//
+// Created by w15627 on 2019/12/19.
+//
+
+#include <iostream>
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+int main(int argc, char *argv[])
+{
+        cout << "Shit" << endl;
+        cout << sizeof(UINT64_MAX);
+
+        return 0;
+}
